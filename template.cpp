5b50803e117b17ff4991d9e583241b36f1bc415a
gutsimoes <gus.ramos.simoes@gmail.com>
2025-04-15 16:38:39 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define vi vector<int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ii pair<int,int>
+
+// alias gr='g++ main.cpp -o main && ./main'
+
+int main(){
+     
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //
+
+    
+    
+    return 0;
+}
